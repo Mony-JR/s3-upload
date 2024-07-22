4b2d45120c286e21ebd88b5342d8987debc3619d
Mony-JR <bronyfb2000@gmail.com>
2024-07-22 13:19:54 +0700

    @1.0.4

--- src/__test__/setup.ts
+++ src/__test__/setup.ts
@@ -2,5 +2,5 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-const envPath = path.resolve(__dirname, '../configs/.env.s3');
+const envPath = path.resolve(__dirname, '../configs/.env.test');
 dotenv.config({ path: envPath });
